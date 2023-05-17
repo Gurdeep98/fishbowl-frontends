38e13b2b1be5ed5dd4bbec9074b0b72717d48787
gurdeep98 <prajapatigurdeep198@gmail.com>
2023-05-17 22:21:06 +0530

    Add grid

--- src/Component/Dashbord1.js
+++ src/Component/Dashbord1.js
@@ -1,44 +1,110 @@
-import React from 'react'
-import {Box, TableContainer,Table, TableBody, TableCell,TableRow, Paper } from '@mui/material'
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { styled } from "@mui/system";
 
 const useStyles = styled({
-    table: {
-      minWidth: 650,
-    },
-  });
+  gridItem: {
+    width: '300px',
+    height: '200px',
+    border: '1px solid black',
+    borderRadius: '10px',
+    padding: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '24px',
+  },
+});
 export default function Dashbord1() {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <>
-        <Box p={2}>
-            <h2>Small Pack</h2>
+      <Box p={2} >
+        <h2>Small Pack</h2>
+      </Box>
 
-        </Box>
-        <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="inventory table">
-        <TableBody>
-        <TableRow >
-        <TableCell align="right">Counter Sale</TableCell>
-        <TableCell align="right">Next Day Air</TableCell>
-        <TableCell align="right">Ship Today</TableCell>
-
-            </TableRow>
-            <TableRow >
-        <TableCell align="right">Pickable</TableCell>
-        <TableCell align="right">Picking</TableCell>
-        <TableCell align="right">Packable</TableCell>
-
-            </TableRow>
-            <TableRow >
-        <TableCell align="right">Packed Today</TableCell>
-        <TableCell align="right">Packed this Week</TableCell>
-        <TableCell align="right">Packed this Month</TableCell>
-
-            </TableRow>
-            </TableBody>
-        </Table>
-        </TableContainer>
+      <Grid container spacing={2}>
+        <Grid item xs={4} className={classes.gridItem} sx={{ bgcolor: 'red' }}>
+          <Typography variant="body1" align="center">
+          Counter Sale
+          </Typography>
+          <Typography variant="caption" align="center">
+            1
+          </Typography>
+        </Grid>
+        <Grid item xs={4} className={classes.gridItem} sx={{ bgcolor: 'green' }}>
+          <Typography variant="body1" align="center">
+          Counter Sale
+          </Typography>
+          <Typography variant="caption" align="center">
+            2
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: 'red' }}>
+          <Typography variant="body1" align="center">
+          Ship Today
+          </Typography>
+          <Typography variant="caption" align="center">
+            3
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: 'yellow' }}>
+          <Typography variant="body1" align="center">
+          Pickable
+          </Typography>
+          <Typography variant="caption" align="center">
+            4
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: 'red' }}>
+          <Typography variant="body1" align="center">
+          Picking
+          </Typography>
+          <Typography variant="caption" align="center">
+            5
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: 'green' }}>
+          <Typography variant="body1" align="center">
+          Packable
+          </Typography>
+          <Typography variant="caption" align="center">
+            6
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: '#87CEEB' }}>
+          <Typography variant="body1" align="center">
+          Packed Today
+          </Typography>
+          <Typography variant="caption" align="center">
+            7
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: '#87CEEB' }}>
+          <Typography variant="body1" align="center">
+          Packed this Week
+          </Typography>
+          <Typography variant="caption" align="center">
+            8
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ bgcolor: '#87CEEB' }}>
+          <Typography variant="body1" align="center">
+          Packed this Month
+          </Typography>
+          <Typography variant="caption" align="center">
+            9
+          </Typography>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
